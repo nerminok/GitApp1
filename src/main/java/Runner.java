319d3d0279704c707c98e1f523a0e7313073fd0d
nermin okuyucu <nerminmercan.45@gmail.com>
2024-05-25 00:38:34 +0300

    v3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,13 @@ public class Runner {
     public static void main(String[] args) {
         //versiyon 1
         System.out.println("hello GİT");
+
+
+        //versiyon 2
+        System.out.println("git add . : staging areaya alir.");
+
+        //versiyon 3
+        System.out.println("gecmise yolculuk");
     }
 
 }
